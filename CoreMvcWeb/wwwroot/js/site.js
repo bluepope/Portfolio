8d9bfd76e2a209d430bfe90f70d5157923e4ad27
안현모 <blue_pope@hotmail.com>
2019-06-28 16:48:10 +0900

    Ajax Upload Progress 관련 TypeScript 추가 Hello TypeScript 삭제

--- CoreMvcWeb/wwwroot/js/site.js
+++ CoreMvcWeb/wwwroot/js/site.js
@@ -1,3 +1,24 @@
+var DragDrop = /** @class */ (function () {
+    function DragDrop() {
+    }
+    DragDrop.SetFileDropZone = function (cssSelector, callBack) {
+        var dropZoneList = document.querySelectorAll(cssSelector);
+        Array.prototype.slice.call(dropZoneList).forEach(function (dropZone) {
+            dropZone.addEventListener('dragover', function (e) {
+                e.stopPropagation();
+                e.preventDefault();
+                e.dataTransfer.dropEffect = 'copy';
+            });
+            // Get file data on drop
+            dropZone.addEventListener('drop', function (e) {
+                e.stopPropagation();
+                e.preventDefault();
+                callBack(e, e.dataTransfer.files);
+            });
+        });
+    };
+    return DragDrop;
+}());
 var AjaxCommonError = function (xhr) {
     var titleDiv = "<div class=\"titleerror\">";
     if (xhr.statusText === "abort")
@@ -5,7 +26,7 @@ var AjaxCommonError = function (xhr) {
     else if (xhr.responseType == "json") {
         alert(xhr.responseText);
     }
-    else if (xhr.responseText.indexOf(titleDiv) > -1) {
+    else if (xhr.responseText != null && xhr.responseText.indexOf(titleDiv) > -1) {
         var message = xhr.responseText.substring(xhr.responseText.indexOf(titleDiv) + titleDiv.length, xhr.responseText.indexOf("</div>"));
         var div = document.createElement("div");
         div.innerHTML = message;
@@ -31,39 +52,86 @@ var AjaxCommonError = function (xhr) {
             }
         });
 */ 
-var Hello = /** @class */ (function () {
-    function Hello(name) {
-        if (name) {
-            this.name = name;
-        }
-        else {
-            this.name = "no data";
-        }
+var JQueryAjaxProgress = /** @class */ (function () {
+    function JQueryAjaxProgress() {
     }
-    Hello.prototype.sayHello = function () {
-        return "Hello, " + this.name;
+    JQueryAjaxProgress.prototype.SetButtonSendProgress = function (buttonProgressObject) {
+        this._buttonProgressObject = buttonProgressObject;
+        this._buttonProgressObject.InitProgress();
     };
-    return Hello;
+    JQueryAjaxProgress.prototype.GetXhr = function () {
+        var _this = this;
+        var customXhr = $.ajaxSettings.xhr();
+        if (customXhr.upload) { // check if upload property exists
+            if (this.sendProgressEvent || this._buttonProgressObject) {
+                customXhr.upload.addEventListener('progress', function (evt) {
+                    if (_this._buttonProgressObject) {
+                        if (evt.lengthComputable) {
+                            var percentComplete = Math.round((evt.loaded / evt.total) * 100);
+                            _this._buttonProgressObject.SetProgress(percentComplete);
+                        }
+                    }
+                    if (_this.sendProgressEvent) {
+                        _this.sendProgressEvent(evt);
+                    }
+                }, false); // for handling the progress of the upload
+            }
+            if (this.sendAbortEvent) {
+                customXhr.upload.addEventListener('abort', this.sendAbortEvent); // for handling the progress of the upload
+            }
+            if (this.sendCompleteEvent || this._buttonProgressObject) {
+                customXhr.upload.addEventListener('load', function (evt) {
+                    if (_this._buttonProgressObject) {
+                        _this._buttonProgressObject.EndProgress();
+                    }
+                    if (_this.sendCompleteEvent) {
+                        _this.sendCompleteEvent(evt);
+                    }
+                }); // for handling the progress of the upload
+            }
+        }
+        return customXhr;
+    };
+    return JQueryAjaxProgress;
 }());
-var DragDrop = /** @class */ (function () {
-    function DragDrop() {
+var ButtonProgress = /** @class */ (function () {
+    function ButtonProgress(target) {
+        this._targetObject = target;
+        this.InitProgress();
     }
-    DragDrop.SetFileDropZone = function (cssSelector, callBack) {
-        var dropZoneList = document.querySelectorAll(cssSelector);
-        Array.prototype.slice.call(dropZoneList).forEach(function (dropZone) {
-            dropZone.addEventListener('dragover', function (e) {
-                e.stopPropagation();
-                e.preventDefault();
-                e.dataTransfer.dropEffect = 'copy';
-            });
-            // Get file data on drop
-            dropZone.addEventListener('drop', function (e) {
-                e.stopPropagation();
-                e.preventDefault();
-                callBack(e, e.dataTransfer.files);
-            });
-        });
+    ButtonProgress.prototype.InitProgress = function () {
+        this._initFlag = 0;
+        this._progressPercent = 0;
+        this._color = window.getComputedStyle(this._targetObject, "").backgroundColor;
+        //console.log(this._color);
+        if (this._color == "transparent" || this._color == "rgba(0, 0, 0, 0)") {
+            this._color = "#007BFF";
+        }
+        this._targetObject.style.color = "black";
+        this._targetObject.style.background = "linear-gradient(to right, " + this._color + " " + this._progressPercent.toString() + "%, transparent 0%)";
+        if (this._targetObject.tagName == "BUTTON") {
+            this._targetObject.setAttribute("disabled", "true");
+        }
     };
-    return DragDrop;
+    ButtonProgress.prototype.SetProgress = function (percent) {
+        this._initFlag = 1;
+        if (this._progressPercent != percent) {
+            this._progressPercent = percent;
+            this._targetObject.style.background = "linear-gradient(to right, " + this._color + " " + this._progressPercent.toString() + "%, transparent 0%)";
+            if (this._progressPercent >= 100) {
+                this._targetObject.classList.add("blinking");
+            }
+        }
+    };
+    ButtonProgress.prototype.EndProgress = function () {
+        this._initFlag = 2;
+        this._targetObject.style.color = "";
+        this._targetObject.style.background = "";
+        this._targetObject.classList.remove("blinking");
+        if (this._targetObject.tagName == "BUTTON") {
+            this._targetObject.removeAttribute("disabled");
+        }
+    };
+    return ButtonProgress;
 }());
 //# sourceMappingURL=site.js.map
