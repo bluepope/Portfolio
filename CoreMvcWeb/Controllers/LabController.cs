12133a56e1480b848ccdc37fb6669d16e1cecb38
안현모 <blue_pope@hotmail.com>
2019-07-08 15:29:43 +0900

    System.Drawing.Common의 v4.5 종속 문제에 대한 주석 추가

--- CoreMvcWeb/Controllers/LabController.cs
+++ CoreMvcWeb/Controllers/LabController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CoreMvcWeb.Models;
+using System.Drawing;
 
 namespace CoreMvcWeb.Controllers
 {
@@ -18,6 +19,11 @@ namespace CoreMvcWeb.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 바코드 제네레이터
+        /// </summary>
+        /// <param name="barcode_number"></param>
+        /// <returns></returns>
         public IActionResult BarCodeGenerator(string barcode_number)
         {
             if (barcode_number.IsNull() == false)
@@ -25,6 +31,15 @@ namespace CoreMvcWeb.Controllers
                 try
                 {
                     ViewData["barcode_number"] = barcode_number;
+
+                    //바코드는 System.Drawing.Common 을 사용하는데 Core 2.2 기준으로 현재 CrossPlatform 지원 안됨
+                    //core 3.0에서 해결될 것으로 기대 중
+
+                    //이를 해결하기 위해 Microsoft.Windows.Compatibility 를 Nuget 에서 설치
+                    // centos 기준으로 다음 작업 필요
+                    // yum install -y epel-release 
+                    // yum whatprovides libgdiplus --최신버전 검색
+                    // yum install -y libgdiplus-2.10-10.el7.x86_64 --19.07.08 기준 최신
 
                     var barcode = new NetBarcode.Barcode(barcode_number, NetBarcode.Type.EAN13, true);
 
