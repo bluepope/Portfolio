0dbd28f13236c65bf83ae04304b712697f5801fd
안현모 <blue_pope@hotmail.com>
2019-07-10 21:58:41 +0900

    메뉴 active 랑 noti count 적용

--- CoreMvcWeb/Controllers/ViewComponents/LeftMenu.cs
+++ CoreMvcWeb/Controllers/ViewComponents/LeftMenu.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CoreMvcWeb.Hubs.Chat;
+using CoreMvcWeb.Models.Common;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +12,73 @@ namespace CoreMvcWeb.Controllers.ViewComponents
 {
     public class LeftMenu : ViewComponent
     {
-        public LeftMenu()
-        {
+        static IHubContext<ChatHub> ChatHub;
 
+        public LeftMenu(IHubContext<ChatHub> chatHub)
+        {
+            ChatHub = chatHub;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return await Task.Run(() => { return View("Default"); });
+            var controller = ViewContext.RouteData.Values["Controller"] as string;
+            var action = ViewContext.RouteData.Values["Action"] as string;
+
+            var list = new List<LeftFolderModel>();
+            list.Add(LeftFolderModel.Create((setting) => {
+                setting.ControllerName = "realtime";
+                setting.Name = "RealTime";
+
+                setting.SubMenuList.Add(LeftMenuModel.Create((menu) => {
+                    menu.ActionName = "chat";
+                    menu.Name = "채팅";
+                    //menu.NotiCount = 1; signalr 은 hub clients count 가 없음 별도로 구현해야함
+                }));
+            }));
+
+            list.Add(LeftFolderModel.Create((setting) => {
+                setting.ControllerName = "clientside";
+                setting.Name = "Client Side";
+
+                setting.SubMenuList.Add(LeftMenuModel.Create((menu) => {
+                    menu.ActionName = "vue";
+                    menu.Name = "VueJs";
+                }));
+
+                setting.SubMenuList.Add(LeftMenuModel.Create((menu) => {
+                    menu.ActionName = "ajaxTest";
+                    menu.Name = "Ajax Test";
+                }));
+
+                setting.SubMenuList.Add(LeftMenuModel.Create((menu) => {
+                    menu.ActionName = "DragAndDropMultipleupload";
+                    menu.Name = "드래그앤드롭";
+                }));
+            }));
+
+            list.Add(LeftFolderModel.Create((setting) => {
+                setting.ControllerName = "board";
+                setting.Name = "게시판";
+
+                setting.SubMenuList.Add(LeftMenuModel.Create((menu) => {
+                    menu.ActionName = "list";
+                    menu.Name = "리스트";
+                }));
+            }));
+
+            list.Add(LeftFolderModel.Create((setting) => {
+                setting.ControllerName = "Lab";
+                setting.Name = "실험실";
+
+                setting.SubMenuList.Add(LeftMenuModel.Create((menu) => {
+                    menu.ActionName = "BarCodeGenerator";
+                    menu.Name = "바코드 생성";
+                }));
+            }));
+
+            list.FirstOrDefault(p => p.ControllerName.ToLower() == controller.ToLower())?.SubMenuList.FirstOrDefault(p => p.ActionName.ToLower() == action.ToLower())?.setActive(true);
+
+            return await Task.Run(() => { return View("Default", list); });
         }
     }
 }
---
+++ CoreMvcWeb/Models/Common/LeftMenuModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMvcWeb.Models.Common
+{
+    public class LeftMenuModel
+    {
+        public LeftMenuModel() { }
+        public static LeftMenuModel Create(Action<LeftMenuModel> setting)
+        {
+            var model = new LeftMenuModel();
+            setting.Invoke(model);
+
+            return model;
+        }
+
+        public string ActionName { get; set; }
+        public string Name { get; set; }
+        public bool isActive { get; set; }
+
+        public int NotiCount { get; set; }
+
+        public void setActive(bool flag) => this.isActive = flag;
+        public void setNotiCount(int notiCount) => this.NotiCount = notiCount;
+    }
+}
