03a5fb41ab674549805ffcdc0c3dcd29efa4b13a
안현모 <blue_pope@hotmail.com>
2019-12-22 01:20:35 +0900

    갈아엎고 재조정중..

--- BluePope.HomeWeb/wwwroot/lib/ElaAdmin/js/scripts.js
+++ BluePope.HomeWeb/wwwroot/lib/ElaAdmin/js/scripts.js
@@ -9,8 +9,6 @@ $(function () {
             $(".navbar-brand span").hide(), $(".scroll-sidebar, .slimScrollDiv").css("overflow-x", "visible").parent().css("overflow", "visible"),
             $(".sidebartoggler i").addClass("ti-menu")) : ($("body").removeClass("mini-sidebar"),
                 $(".navbar-brand span").show());
-        var i = (window.innerHeight > 0 ? window.innerHeight : this.screen.height) - 1;
-        (i -= 70) < 1 && (i = 1), i > 70 && $(".page-wrapper").css("min-height", i + "px");
     };
 
 
